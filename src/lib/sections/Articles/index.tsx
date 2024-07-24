7727ebf318d7100ff3637679511eeeac869d4c3b
Sarah Renfro <srenfro00@gmail.com>
2024-07-24 16:01:30 -0500

    Improve articles section

--- src/lib/sections/Articles/index.tsx
+++ src/lib/sections/Articles/index.tsx
@@ -1,5 +1,3 @@
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { map } from 'fp-ts/lib/Array'
 import { Card, SectionHeading } from '~/lib/components'
 import { Heading } from '~/lib/typography'
@@ -7,29 +5,25 @@ import { articles } from './model'
 import { Article } from './types'
 
 const toArticle = ({ title, link, imgSrc }: Article) => (
-  <li key={title}>
-    <Card variant="withDivider" className="m-6">
-      <img src={imgSrc} className='w-full'/>
-      <a
-        href={link}
-        className="flex items-center justify-center flex-1 p-2"
-      >
-        <Heading level="h3" className="text-center m-4">
+  <li key={title}  className="m-6">
+    <a href={link}>
+      <Card variant="withDivider">
+        <img src={imgSrc} className="w-full" />
+        <Heading level="h3" className="text-center p-4">
           {title}
-          <FontAwesomeIcon
-            icon={faArrowUpRightFromSquare}
-            size="xs"
-            className='ml-2'
-          />
         </Heading>
-      </a>
-    </Card>
+      </Card>
+    </a>
   </li>
 )
 
 export const Articles = () => (
   <>
-    <SectionHeading variant="simple" className="mt-12" id="articles">
+    <SectionHeading
+      variant="simple"
+      className="mt-12"
+      id="articles"
+    >
       <Heading level="h3">Articles</Heading>
     </SectionHeading>
     <ul
